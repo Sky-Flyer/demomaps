00ee6291eec5534b5f980c923a80c0a7fc54ef33
Andreas Schweitzer <sky-flyer@gmx.net>
2016-10-03 13:05:38 +0200

    add typscript test project

---
+++ typescripttest/helloWorld.js
@@ -0,0 +1,12 @@
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function (name) {
+        console.log('Hello ' + name);
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main("Andreas");
+//Startup.main(4711);  
+//# sourceMappingURL=helloWorld.js.map
---
+++ typescripttest/helloWorld.ts
@@ -0,0 +1,9 @@
+class Startup {
+    public static main(name: String): number {
+        console.log('Hello '+name);
+        return 0;
+    }
+}
+
+Startup.main("Andreas");
+//Startup.main(4711); 
